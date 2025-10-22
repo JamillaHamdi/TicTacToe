8640c85e55f659230bb61fb9c5b172acb84f754b
sc <xldsx@duck.com>
2025-10-22 13:40:25 +0200

    Val metod klar.

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
+
     public static void main(String[] args) {
-    
+        //Spelare 1 väljer
+        player1ValjXellerOClass.player1ValjXellerOMetod(args);
+
     }
+
 }
---
+++ player1ValjXellerOClass.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class player1ValjXellerOClass {
+    public static Scanner scanner = new Scanner(System.in);
+    public static void player1ValjXellerOMetod(String[] args) {
+
+        System.out.println("Spelare 1, Välj O eller X");
+        String player1Val = scanner.next();
+        if (player1Val.equalsIgnoreCase("x")) {
+
+            System.out.println("Okej, spelare 1 är X ");
+
+        } else if (player1Val.equalsIgnoreCase("o")) {
+
+            System.out.println("Okej, spelare 1 är O ");
+
+        } else {
+
+            System.out.println("Mannen välj rätt");
+            
+        }
+
+        
+
+
+    }
+}
