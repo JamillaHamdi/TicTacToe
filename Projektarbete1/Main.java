cdaf385a471fe7cd4f512b9706cdff5c9c1ee446
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-10-25 13:35:07 +0200

    Sparad mainklass

---
+++ Projektarbete1/Main.java
@@ -0,0 +1,11 @@
+package Projektarbete1;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        //Spelare 1 väljer
+
+        player1ValjXellerOClass.player1ValjXellerOMetod(args);
+    }
+
+}
