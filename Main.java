e5395316e5ce06271e06e9d16247fc18c270c0b7
sc <xldsx@duck.com>
2025-10-27 14:45:00 +0100

    Bräd nummren är nu strings.

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.*;;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         player1valjXellerOClass.player1ValjXellerOMetod(args);
+        printBoardClass.printBoardMetod(args);
     }
 
 }
